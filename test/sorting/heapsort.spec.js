2b1b13f241904533ed174835bb7a943f18af2aba
Andrew <akv9509@saintjoe.edu>
2015-04-15 13:47:19 -0400

    Updated to test heapsort, just needs to display array better

--- test/sorting/heapsort.spec.js
+++ test/sorting/heapsort.spec.js
@@ -1,5 +1,5 @@
 //var sortTestCase = require('./sort.testcase.js');
-var heapSort = require('../../src/sorting/heapsort.js');
+//var heapSort = require('../../src/sorting/heapsort.js');
 
 //sortTestCase(heapSort, 'Heap sort');
 
@@ -8,15 +8,33 @@ var heapSort = require('../../src/sorting/heapsort.js');
 
 //load('heapsort.js');
 
+//var arr = [];
+//var MAX = 100
+
+//for(var i = 0; i < MAX; ++i){
+//		arr[i] = Math.floor((Math.random() * 100) + 1);
+	//	print(arr[i]);	
+//}
+//print(arr);
+
+
+
+//heapSort(arr);
+
+
 var arr = [];
-var MAX = 100
+var MAX = 100;
+
+var sort = require('./../../src/sorting/heapsort.js').heapSort;
+console.log(sort([2, 5, 1, 0, 4])); // [ 0, 1, 2, 4, 5 ]
+
 
 for(var i = 0; i < MAX; ++i){
-		arr[i] = Math.floor((Math.random() * 100) + 1);
-	//	print(arr[i]);	
+      arr[i] = Math.floor((Math.random() * 100) + 1);
+    //  print(arr[i]);
 }
-print(arr);
+console.log(arr);
+sort(arr);
+console.log(arr);
 
 
-
-heapSort(arr);
